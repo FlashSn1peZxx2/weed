d7dfe77b1a1a29d2bccc0865fdb8c76862644294
FlashSn1peZxx2 <69518234+FlashSn1peZxx2@users.noreply.github.com>
2021-12-21 22:50:35 +0100

    Add files via upload

---
+++ esx_drugs/config.lua
@@ -0,0 +1,23 @@
+Config = {}
+
+Config.Locale = 'en'
+
+Config.Delays = {
+	WeedProcessing = 1000 * 10
+}
+
+Config.DrugDealerItems = {
+	weed = 91
+}
+
+Config.LicenseEnable = false -- enable processing licenses? The player will be required to buy a license in order to process drugs. Requires esx_license
+
+Config.LicensePrices = {
+	weed_processing = {label = _U('license_weed'), price = 15000}
+}
+
+Config.GiveBlack = true -- give black money? if disabled it'll give regular cash.
+
+Config.CircleZones = {
+	WeedField = {coords = vector3(-277.2, -1632.42, 31.85), name = _U('blip_weedfield'), color = 25, sprite = 496, radius = 0.0},
+}
